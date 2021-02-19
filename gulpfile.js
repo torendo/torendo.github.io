43ad5a861583aff3db2bd8c3f00c4205be95e681
nekro20 <toren4eg@gmail.com>
2021-02-19 17:25:46 +0300

    actualized

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
 const gulp = require('gulp');
+var concat = require('gulp-concat');
 
-module.exports.build = function () {
-  return gulp.src('./node_modules/materialize-css/dist/css/materialize.css')
-    .pipe(gulp.dest('./css'));
-};
+function css (isDev) {
+  var sheets = [
+    './node_modules/materialize-css/dist/css/materialize.css',
+    './src/main.css'
+  ];
+  if (isDev) {
+    sheets.push('./src/print-test.css');
+  }
+  return gulp.src(sheets)
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./'));
+}
+
+function html () {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./'));
+}
+
+var build = gulp.parallel(
+  css.bind(null, false),
+  html
+);
+
+exports.build = build;
+
+var buildDev = gulp.parallel(
+  css.bind(null, true),
+  html
+);
+
+exports.buildDev = buildDev;
